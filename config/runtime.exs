2c3a05ca72efbaf8f57ed29fa1c55adb8ca3b785
Khaja Minhajuddin <minhajuddin.k@gmail.com>
2021-10-21 22:32:35 -0400

    fly.io setup

--- config/runtime.exs
+++ config/runtime.exs
@@ -18,6 +18,8 @@ if config_env() == :prod do
     # ssl: true,
     # socket_options: [:inet6],
     url: database_url,
+    # IMPORTANT: Or it won't find the DB server
+    socket_options: [:inet6],
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
@@ -33,13 +35,12 @@ if config_env() == :prod do
       """
 
   config :fasthttp, FastHTTPWeb.Endpoint,
+    server: true,
+    url: [host: "#{app_name}.fly.dev", port: 80],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: String.to_integer(System.get_env("PORT") || "4000"),
+      # IMPORTANT: support IPv6 addresses
+      transport_options: [socket_opts: [:inet6]]
     ],
     secret_key_base: secret_key_base
 
